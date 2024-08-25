1843f464c303021b394da4fe989efb39f46126cb
rzkmufid <rizkimufid.rm@gmail.com>
2024-08-26 02:30:49 +0700

    update view planning

--- src/Page/WorkPlanning/ViewPlanning.jsx
+++ src/Page/WorkPlanning/ViewPlanning.jsx
@@ -1,31 +1,40 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from 'react-router-dom';
-import { Box, Flex, Text, Breadcrumb, BreadcrumbItem, BreadcrumbLink, SimpleGrid } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import {
+  Box,
+  Flex,
+  Text,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  SimpleGrid,
+} from "@chakra-ui/react";
 import Sidebar from "./../Components/Sidebar";
 import Navbar from "./../Components/Navbar";
 import { Outlet, useLocation } from "react-router-dom";
-import Plot from 'react-plotly.js';
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
+import Plot from "react-plotly.js";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
 
 const ViewPlanning = () => {
   const { id } = useParams();
-  const [selectedNav, setSelectedNav] = useState('homeDash');
-  const [pageForm, setPageForm] = useState('');
+  const [selectedNav, setSelectedNav] = useState("homeDash");
+  const [pageForm, setPageForm] = useState("");
   const [apiData, setApiData] = useState(null);
+  const [wellCasingImage, setWellCasingImage] = useState(null);
   const location = useLocation();
 
   const handleNavClick = (value) => {
     setSelectedNav(value);
-    setPageForm('');
+    setPageForm("");
   };
 
   useEffect(() => {
     const path = location.pathname;
     if (path.includes("/dashboard/submission")) {
       setPageForm("submission");
-      setSelectedNav('submission');
+      setSelectedNav("submission");
     } else if (path.includes("/dashboard/operasi")) {
       setPageForm("operations");
       setSelectedNav("operations");
@@ -39,18 +48,36 @@ const ViewPlanning = () => {
       setPageForm("");
     }
 
-    // Fetch data from API
+    // Fetch data dari API
     const fetchData = async () => {
       try {
-        const response = await fetch(`http://127.0.0.1:8000/job/planning/view/${id}/`, {
-          headers: {
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJza2siLCJleHAiOjE3MjQ1MTQxMjB9.T5KGGpdlLRr7EmBnrjAs-XWEEOOhAPRxD_NH6zm3_lc',
-            'accept': 'application/json'
+        const response = await fetch(
+          `http://127.0.0.1:8000/job/planning/view/${id}/`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+              accept: "application/json",
+            },
           }
-        });
+        );
 
         const data = await response.json();
         setApiData(data);
+
+        if (data.well_casing && data.well_casing.path) {
+          const casingResponse = await fetch(
+            `http://127.0.0.1:8000${data.well_casing.path}`,
+            {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+                accept: "application/json",
+              },
+            }
+          );
+          const blob = await casingResponse.blob();
+          const imageUrl = URL.createObjectURL(blob);
+          setWellCasingImage(imageUrl);
+        }
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -66,19 +93,80 @@ const ViewPlanning = () => {
     </Box>
   );
 
-  const renderOperationalDetails = (data) => (
+  const renderDetails = (title, data) => (
     <Box flex="1" mr={4} mb={8}>
-      <Text fontSize="xl" fontWeight="bold" mb={4}>Operational Details</Text>
-      {Object.entries(data).map(([key, value]) => renderCardItem(key, value))}
+      <Text fontSize="xl" fontWeight="bold" mb={4}>
+        {title}
+      </Text>
+      {data && Object.keys(data).length > 0 ? (
+        <SimpleGrid columns={2}>
+          {Object.entries(data).map(([key, value]) =>
+            renderCardItem(key, value)
+          )}
+        </SimpleGrid>
+      ) : (
+        <Text>Data tidak tersedia</Text>
+      )}
     </Box>
   );
 
-  const renderTechnicalDetails = (data) => (
-    <Box flex="1" mr={4} mb={8}>
-      <Text fontSize="xl" fontWeight="bold" mb={4}>Technical Details</Text>
-      <SimpleGrid columns={2}  >
-        {Object.entries(data).map(([key, value]) => renderCardItem(key, value))}
-      </SimpleGrid>
+  const renderPlot = (title, plotData, layout) => (
+    <Box mb={8} w="100%">
+      <Text fontSize="xl" fontWeight="bold" mb={4}>
+        {title}
+      </Text>
+      {plotData && plotData.length > 0 ? (
+        <Plot
+          data={plotData}
+          layout={{
+            ...layout,
+            width: undefined, // Gunakan lebar penuh dari kontainer komponen
+            autosize: true, // Mengaktifkan autosize
+          }}
+          useResizeHandler
+          style={{ width: "100%", height: "100%" }} // Memastikan plot memenuhi lebar kontainer
+        />
+      ) : (
+        <Text>Data plot tidak tersedia</Text>
+      )}
+    </Box>
+  );
+
+  const transformJobOperationData = (tableData) => {
+    const rowCount = Object.keys(tableData.Event).length;
+    const transformedData = [];
+
+    for (let i = 0; i < rowCount; i++) {
+      transformedData.push({
+        Event: tableData.Event[i],
+        Days: tableData.Days[i],
+        "Start Depth": tableData["Start Depth"][i],
+        "End Depth": tableData["End Depth"][i],
+        Cost: tableData.Cost[i],
+      });
+    }
+
+    return transformedData;
+  };
+
+  const renderAgGrid = (title, rowData, columnDefs) => (
+    <Box flex="1" mb={8}>
+      <Text fontSize="xl" fontWeight="bold" mb={4}>
+        {title}
+      </Text>
+      {rowData && rowData.length > 0 ? (
+        <div
+          className="ag-theme-alpine"
+          style={{ height: "400px", width: "100%" }}
+        >
+          <AgGridReact
+            rowData={rowData}
+            columnDefs={columnDefs.map((colDef) => ({ ...colDef, flex: 1 }))}
+          />
+        </div>
+      ) : (
+        <Text>Data tabel tidak tersedia</Text>
+      )}
     </Box>
   );
 
@@ -86,16 +174,18 @@ const ViewPlanning = () => {
     <Flex>
       <Sidebar handleMenuValue={handleNavClick} selectedNav={selectedNav} />
       <Box flex="1" p={4}>
-        <Navbar appName="View Planning"/>
+        <Navbar appName="View Planning" />
         <Box mt={4}>
           <Breadcrumb mb={4}>
             <BreadcrumbItem>
-              <BreadcrumbLink href='/'>View Planning</BreadcrumbLink>
+              <BreadcrumbLink href="/">View Planning</BreadcrumbLink>
             </BreadcrumbItem>
-            {pageForm === 'submission' && (
+            {pageForm === "submission" && (
               <BreadcrumbItem>
-                <BreadcrumbLink href='/dashboard'>Exploration</BreadcrumbLink>
-                <BreadcrumbLink href='#' fontWeight={'bold'}>Submission</BreadcrumbLink>
+                <BreadcrumbLink href="/dashboard">Exploration</BreadcrumbLink>
+                <BreadcrumbLink href="#" fontWeight={"bold"}>
+                  Submission
+                </BreadcrumbLink>
               </BreadcrumbItem>
             )}
           </Breadcrumb>
@@ -105,40 +195,85 @@ const ViewPlanning = () => {
           {apiData && (
             <>
               {/* Row 1: Operational and Technical Cards */}
-              <Flex>
-                {renderOperationalDetails(apiData.operational)}
-                {renderTechnicalDetails(apiData.technical)}
+              <Flex mb={4}>
+                {renderDetails("Operational Details", apiData.operational)}
+                {renderDetails("Technical Details", apiData.technical)}
               </Flex>
 
-              {/* Row 2: Work Breakdown Structure Table and Chart */}
-              <Box mb={8}>
-                <Text fontSize="xl" fontWeight="bold" mb={4}>Work Breakdown Structure</Text>
-                <Flex>
-                  <Box flex="1" mr={4}>
-                    <div className="ag-theme-alpine" style={{ height: '400px', width: '100%' }}>
-                      {apiData.work_breakdown_structure && apiData.work_breakdown_structure.table && (
-                        <AgGridReact
-                          rowData={apiData.work_breakdown_structure.table} // Data untuk tabel
-                          columnDefs={Object.keys(apiData.work_breakdown_structure.table[0] || {}).map(key => ({
-                            headerName: key,
-                            field: key
-                          }))} // Definisi kolom berdasarkan keys dari objek data
-                        />
-                      )}
-                    </div>
-                  </Box>
-                  <Box flex="1">
-                    {apiData.work_breakdown_structure && apiData.work_breakdown_structure.plot && (
-                      <Plot
-                        data={apiData.work_breakdown_structure.plot.data || []}
-                        layout={apiData.work_breakdown_structure.plot.layout || {}}
-                        useResizeHandler
-                        style={{ width: '100%', height: '100%' }}
+              {/* Row 2: Job Operation Days Table and Plot */}
+              {apiData.job_operation_days && (
+                <>
+                  {renderAgGrid(
+                    "Job Operation Days Table",
+                    transformJobOperationData(apiData.job_operation_days.table),
+                    Object.keys(apiData.job_operation_days.table).map(
+                      (key) => ({
+                        headerName: key,
+                        field: key,
+                      })
+                    )
+                  )}
+                  {renderPlot(
+                    "Job Operation Days Plot",
+                    apiData.job_operation_days.plot.data,
+                    apiData.job_operation_days.plot.layout
+                  )}
+                </>
+              )}
+
+              {/* Row 3: Work Breakdown Structure Table and Plot */}
+              {apiData.work_breakdown_structure && (
+                <>
+                  {renderAgGrid(
+                    "Work Breakdown Structure Table",
+                    apiData.work_breakdown_structure.table,
+                    Object.keys(
+                      apiData.work_breakdown_structure.table[0] || {}
+                    ).map((key) => ({
+                      headerName: key,
+                      field: key,
+                    }))
+                  )}
+                  {renderPlot(
+                    "Work Breakdown Structure Plot",
+                    apiData.work_breakdown_structure.plot.data,
+                    apiData.work_breakdown_structure.plot.layout,
+                    true
+                  )}
+                </>
+              )}
+
+              {/* Row 4: Well Trajectory and Well Casing */}
+              <Flex mb={8}>
+                <Box flex="1" mr={4} minW="40%">
+                  {apiData.well_trajectory && apiData.well_trajectory.plot ? (
+                    renderPlot(
+                      "Well Trajectory",
+                      apiData.well_trajectory.plot.data,
+                      apiData.well_trajectory.plot.layout
+                    )
+                  ) : (
+                    <Text>Data Well Trajectory tidak tersedia</Text>
+                  )}
+                </Box>
+
+                <Box flex="1" minW="40%">
+                  {apiData.well_casing && apiData.well_casing.path ? (
+                    <Box>
+                      <Text fontSize="xl" fontWeight="bold" mb={4}>
+                        Well Casing
+                      </Text>
+                      <img
+                        src={wellCasingImage}
+                        alt="Well Casing"
+                        style={{ width: "100%" }}
                       />
-                    )}
-                  </Box>
-                </Flex>
-              </Box>
+                    </Box>
+                  ) : (
+                    <Text>Data Well Casing tidak tersedia</Text>
+                  )}
+                </Box>
+              </Flex>
             </>
           )}
 
