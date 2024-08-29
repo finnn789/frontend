3050188cf083682f58a5d45f520194f6854f6ce4
rzkmufid <rizkimufid.rm@gmail.com>
2024-08-29 15:04:52 +0700

    update

--- src/Page/PageSKK/Components/Card/CardPerhitunganBox.jsx
+++ src/Page/PageSKK/Components/Card/CardPerhitunganBox.jsx
@@ -3,42 +3,39 @@ import { Box, Flex, Text, Icon } from "@chakra-ui/react";
 import { FaFileAlt } from "react-icons/fa";
 
 const PerhitunganCard = ({
+  percentage,
   number,
   label,
   subLabel,
   icon = FaFileAlt,
   bgIcon = "blue.100",
   iconColor = "blue.500",
-  percentage,
 }) => {
   return (
     <Box bg="white" boxShadow="md" borderRadius="2xl" p={4} width={"100%"}>
       <Flex alignItems="center" mb={2} gap={5}>
         <Flex borderRadius="full" bg={bgIcon} p={4} mr={2}>
           <Icon as={icon} boxSize={10} color={iconColor} />
-        <Flex borderRadius="full" bg={bgIcon} p={4} mr={2}>
-          <Icon as={icon} boxSize={10} color={iconColor} />
         </Flex>
         <Flex flexDirection={"column"}>
           <Text
-            fontSize="4rem"
+            fontSize="1.5rem"
             color={"gray.600"}
             fontWeight="bold"
             lineHeight="1"
           >
             {number}
           </Text>
-          <Text fontSize="xl" fontWeight="medium" color="gray.700">
-          {label}
-
+          <Text fontSize="28px" fontWeight="medium" textTransform={"uppercase"} color="gray.500">
+            {label}
           </Text>
         </Flex>
       </Flex>
-      <Flex gap={5}>
-        {percentage}
-        <Text fontSize="xl" color="gray.500">
-          {subLabel}
-        </Text>
+      <Flex gap={4}>
+      {percentage}
+      <Text fontSize="xl" color="gray.500">
+        {subLabel}
+      </Text>
       </Flex>
     </Box>
   );
