41f2b1a806dbc1e620e98765b4fd3626a2d91213
finnn789 <aripstar47@gmail.com>
2024-09-07 12:31:48 +0700

    WellChange:

--- src/Page/Forms/PengajuanDrillingForm.jsx
+++ src/Page/Forms/PengajuanDrillingForm.jsx
@@ -66,7 +66,7 @@ const PengajuanDrillingForm = () => {
       rig_name: "string",
       rig_type: "JACK-UP",
       rig_horse_power: 0,
-      well_plan: {
+      well: {
         unit_type: "Metrics",
         uwi: "string",
         field_id: "string",
@@ -213,7 +213,7 @@ const PengajuanDrillingForm = () => {
       ...prevJobPlan,
       job_plan: {
         ...prevJobPlan.job_plan,
-        well_plan: {
+        well: {
           ...wellData,
         },
       },
